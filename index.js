0cdf4650f47b4dd0819a0b7eb485f0ac141bbed1
maryamhaitham <marym.ihnj@gmail.com>
2022-08-29 12:33:29 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Your code here
+
+class Cat {
+    constructor (name, sex){
+        this.name = name;
+        this.sex = sex;
+    }
+    speak(){
+        return `${this.name} says meow!`
+    }
+}
+
+
+class Dog {
+    constructor (name, sex){
+        this.name = name;
+        this.sex = sex;
+    }
+    speak(){
+        return `${this.name} says woof!`
+    }
+}
+
+
+class Bird {
+    constructor (name, sex){
+        this.name = name;
+        this.sex = sex;
+    }
+    speak(){
+        if (this.sex === "male") {return `It's me! ${this.name}, the parrot!`}
+        else return `${this.name} says squawk!`
+    }
+}
+
+const cat = new Cat("Sasha", "female");
+const dog = new Dog("Rufio", "male");
+const bird = new Bird("Pablo", "male");
+const bird2 = new Bird("Mable", "female");
